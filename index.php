c32041733490c6c16a8a25f06e3b734aae1b17fd
BIGStrawberry <wouter.dijkstra.wd@gmail.com>
2017-09-21 16:36:09 +0200

    Index file & test for deployment

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<style>
+			h1   {color: red;}
+			.name {text-align: center;}
+		</style>
+	</head>
+
+	<body>
+		<div class="name">
+			<h1>Avontuur.win</h1>
+		</div>
+	</body>
+</html>
